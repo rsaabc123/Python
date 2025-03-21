a10f2cdbfd9a638cd64d1b0436ea77c9f1b41109
rsaabc123 <yongkc247@gmail.com>
2025-03-21 17:51:21 -0400

    Add files via upload

---
+++ Numbers/math.py
@@ -0,0 +1,13 @@
+#basic math operations
+
+x = 5
+
+r = x + 1 # 5 + 1 -> 2
+r = x - 1 # 5 - 1 -> 4
+r = x * 2 # 5 * 2 -> 10
+r = x / 2 # 5 / 2 -> 2.5
+r = x % 2 # remainder -> 5/2 (resultant = 2; remainder = 1; r =1)
+r = x ** 2 # exponentiation -> 5^2 -> 25
+r = x // 2 # floor division - > 5/2 (resultant -> 2.5 round down -> 2)
+
+#print(r)
